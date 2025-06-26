1b0659203508fd7c45b8077a3e25c0e9382c1551
Corbin Rose <corbinisnotyou@gmail.com>
2025-06-25 22:59:43 -0400

    updates to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ library.add(faBook, faHouse)
       <IonHeader>
         <IonToolbar>
            
-          <IonTitle>Jelly Activities here</IonTitle>
+          <IonTitle>Jelly Activities</IonTitle>
           <IonButtons slot="end"> {/* Toggler on the right slot */}
             <IonMenuButton />
             
@@ -29,46 +29,46 @@ library.add(faBook, faHouse)
           <img src="facebookbanner.png" alt="Jelly Activities Banner" className="img-fluid w-100" />
           
           <div className="hero">
-            <h1 className="text-center">Jelly Activities</h1>
+            <h1 className="text-center display-4 knewave-regular">Jelly Activities</h1>
             <p className="text-center">Explore a variety of fun and educational activities for kids!</p>
           </div>
         </div>
         <div className="container p-0">
         
-        <div className="row">
-          <div className="col-sm-4 mb-3">
-            <div className="card">
+        <div className="rows">
+          <div className="mb-3">
+            <div className="big-button">
               <div className="text-center card-header">
               <IonIcon size="large" icon={bookOutline} />
               </div>
               <div className="card-body">
                 <h5 className="card-title">Coloring Books</h5>
                 <p className="card-text">Explore our collection of coloring books.</p>
-                <a href="/coloring" className="btn btn-primary">Yes!</a>
+                <a href="/coloring" className="btn btn-tertiary">Yes!</a>
               </div>
             </div>
           </div>
-          <div className="col-sm-4 mb-3">
-            <div className="card">
+          <div className="mb-3">
+            <div className="big-button">
               <div className="text-center card-header">
                 <IonIcon size="large" icon={extensionPuzzle} />
                 </div>
               <div className="card-body">
                 <h5 className="card-title">Crossword Puzzles</h5>
                 <p className="card-text">Challenge your mind with our crossword puzzles.</p>
-                <a href="/crossword" className="btn btn-primary">Yes!</a>
+                <a href="/crossword" className="btn btn-tertiary">Yes!</a>
               </div>
             </div>
           </div>
-          <div className="col-sm-4 mb-3">
-            <div className="card">
+          <div className=" mb-3">
+            <div className="big-button">
               <div className="text-center card-header">
               <IonIcon size="large"  icon={informationCircleOutline} />
               </div>
               <div className="card-body">
                 <h5 className="card-title">Trivia</h5>
                 <p className="card-text">Test your knowledge with our trivia questions.</p>
-                <a href="/trivia" className="btn btn-primary">Yes!</a>
+                <a href="/trivia" className="btn btn-tertiary">Yes!</a>
               </div>
             </div>
           </div>
